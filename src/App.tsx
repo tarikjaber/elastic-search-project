334ad5292a30e4c0bb463bb4e17d08e20c6439fb
Tarik Jaber <tariknjaber@gmail.com>
2023-09-08 13:27:56 -0400

    Finally able to connect to elasticsearch

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,19 @@ import './App.css';
 function App() {
   async function checkClusterStatus() {
     const apiUrl = process.env.REACT_APP_API_URL;
+    const key = process.env.REACT_APP_ACCESS_KEY;
+    const secret = process.env.REACT_APP_ACCESS_SECRET;
+    console.log(key)
+    console.log(secret)
+    const base64Credentials = btoa(`${key}:${secret}`);
+
+    const headers = {
+      'Authorization': `Basic ${base64Credentials}`
+    };
+    console.log(apiUrl);
 
     try {
-      const response = await fetch(`${apiUrl}_cluster/health`);
+      const response = await fetch(`${apiUrl}_cluster/health`, { headers });
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
